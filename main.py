3b7012783c72c7a610ebc50b181eb6116a24947f
petertorelli <peter.j.torelli@gmail.com>
2022-06-12 14:25:44 -0700

    First push

---
+++ main.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+
+import time
+from datetime import datetime
+import signal
+import cv2
+import multiprocessing
+from queue import Empty
+from threading import Thread
+import RPi.GPIO as GPIO
+import os
+
+# Motion detector on GPIO 18
+# Floodlight on GPIO 23
+
+class Recorder:
+	def __init__(self):
+		self._state = "idle"
+		self._recording = False
+		self._startTime = 0
+		self._videos = 0
+		self._filename = ""
+
+	def _startVideo(self, name):
+		self._vidCapture = cv2.VideoCapture(0)
+		self._vidCodec = cv2.VideoWriter_fourcc(*'avc1')
+		self._videos = self._videos + 1
+		self._filename = name
+		self._output = cv2.VideoWriter(name, self._vidCodec, 20.0, (640, 480))
+
+	def _stopVideo(self):
+		self._vidCapture.release()
+		self._output.release()
+		cmd = "./pushvideo.sh %s" % self._filename
+		print(cmd)
+		print(os.system(cmd))
+
+	def _lightsOn(self):
+		GPIO.output(23, 1)
+
+	def _lightsOff(self):
+		GPIO.output(23, 0)
+
+	def _changeState(self, state):
+		print("Change from %s to %s" % (self._state, state))
+		# This function just validates the state
+		if (state == 'run'):
+			self._state = 'run'
+		elif state == 'stop':
+			self._state = 'stop'
+		elif state == 'start':
+			self._state = 'start'
+		elif state == 'idle':
+			self._state = 'idle'
+		else:
+			raise Exception("Unknown state %s" % state)
+
+	def run(self, in_q):
+		while True:
+			try:
+				data = in_q.get(False)
+				if data is not None:
+					self._changeState(data)
+			except Empty:
+				pass
+
+			if self._state == 'start':
+				if self._recording is False:
+					self._recording = True
+					dt = datetime.now()
+					self._startVideo(dt.strftime('%Y-%m-%d_%H-%M-%S.mp4'))
+					self._lightsOn()
+					self._startTime = time.time()
+				else:
+					self._startTime = time.time()
+				self._changeState('run')
+			elif self._state == 'run':
+				pass
+			elif self._state == 'stop':
+				if self._recording is True:
+					self._recording = False
+					self._lightsOff()
+					self._stopVideo()
+				self._changeState('idle')
+			elif self._state == 'idle':
+				pass
+
+			if self._recording is True:
+				deltaSec = time.time() - self._startTime	
+				if deltaSec >= 15:
+					self._changeState('stop')
+				else:
+					ret, frame = self._vidCapture.read()
+					self._output.write(frame)
+			else:
+				time.sleep(0.100)
+		
+
+def handler(signum, frame):
+	print("SIGINT")
+	GPIO.cleanup()
+	exit(-1)
+
+def motionHandler(channel):
+	global messageQueue
+	detect = GPIO.input(18)
+	print("edge", time.time(), channel, detect)
+	if detect == 1:
+		messageQueue.put("start")
+
+
+recorder = Recorder();
+messageQueue = multiprocessing.Queue()
+t1 = Thread(target=recorder.run, args=(messageQueue, ), daemon=True)
+t1.start()
+print("Importing done, thread started...")
+
+signal.signal(signal.SIGINT, handler)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.IN);
+GPIO.setup(23, GPIO.OUT)
+GPIO.add_event_detect(18, GPIO.BOTH, callback=motionHandler)
+
+print("Looping in main thread")
+while 1:
+	time.sleep(30)
+	pass
+
+print("Cleaning up main thread")
+GPIO.cleanup()
+
---
+++ pushvideo.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+filename=$1
+
+URL="https://www.repete.io"
+TOKEN_ENDPOINT="${URL}/liddycam/_token"
+POST_ENDPOINT="${URL}/liddycam/_post"
+KEY="head -1 ~/.website-key-upload"
+
+token=`curl --silent $TOKEN_ENDPOINT`
+vtoken=`echo -n ${KEY}${token} | sha256sum | cut -f1 -d' '`
+echo $KEY
+echo $token
+echo $vtoken
+echo $filename
+
+curl \
+	--data-urlencode vtoken=$vtoken \
+	--data-urlencode filename=$filename \
+	--data-urlencode "filedata@${filename}" \
+	$POST_ENDPOINT
+
+
+jpg=${filename%.*}.jpg
+ffmpeg -i $filename -ss 00:00:1.000 -vframes 1 -vf scale=100:-1 $jpg
+
+curl \
+	--data-urlencode vtoken=$vtoken \
+	--data-urlencode filename=$jpg \
+	--data-urlencode "filedata@${jpg}" \
+	$POST_ENDPOINT
+
+rm $jpg
+rm $filename
